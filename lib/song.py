dfaa704d4086c0d81312d9a62fc725912d0e8f69
ngashisaya001-creator <ngashisaya001@gmail.com>
2026-06-09 16:02:10 +0300

    Add Song class with class attributes and class methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
+#!/usr/bin/env python3
+
 class Song:
-    pass
+
+    # Class attributes to track all songs globally
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        # Trigger all class methods on creation
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        # Increment total song count by 1
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        # Add genre only if not already in list
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        # Add artist only if not already in list
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # Increment genre count or initialize to 1
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        # Increment artist count or initialize to 1
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
